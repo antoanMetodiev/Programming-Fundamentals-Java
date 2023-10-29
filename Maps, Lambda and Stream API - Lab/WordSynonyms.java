93a45243853b534a35d5e80839846cb2949ac0f9
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-10-29 22:32:36 +0200

    Update WordSynonyms.java
    delete some single lines

--- Maps, Lambda and Stream API - Lab/WordSynonyms.java
+++ Maps, Lambda and Stream API - Lab/WordSynonyms.java
@@ -1,26 +1,21 @@
 import java.util.*;
 public class WordSynonyms {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         Map<String, List<String>> synonyms = new LinkedHashMap<>();
-
         for (int i = 0; i < n; i++) {
-
+            
             String word = scanner.nextLine();
             String synonym = scanner.nextLine();
-
             if (!synonyms.containsKey(word)){
-
+                
                 List<String> list = new ArrayList<>();
                 list.add(synonym);
                 synonyms.put(word, list);
             }
             else {
-
                 List<String> list = synonyms.get(word);
                 list.add(synonym);
                 synonyms.put(word, list);
@@ -29,7 +24,6 @@ public class WordSynonyms {
         }
 
         for (Map.Entry<String, List<String>> map : synonyms.entrySet()) {
-
             System.out.printf("%s - %s\n", map.getKey(), String.join(", ", map.getValue()));
         }
     }
