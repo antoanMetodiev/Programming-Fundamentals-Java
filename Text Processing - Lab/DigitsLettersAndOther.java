42ec46c046314720805953e8518f57337261e6d9
Antoan Metodiev <Boksnem@abv.bg>
2023-11-03 21:08:40 +0200

    add task

---
+++ Text Processing - Lab/DigitsLettersAndOther.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class DigitsLettersAndOther {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder otherSymbols = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            if (Character.isDigit(symbol)){
+                digits.append(symbol);
+            } else if (Character.isLetter(symbol)){
+                letters.append(symbol);
+            } else {
+                otherSymbols.append(symbol);
+            }
+        }
+        System.out.println(digits);
+        System.out.println(letters);
+        System.out.println(otherSymbols);
+    }
+}
