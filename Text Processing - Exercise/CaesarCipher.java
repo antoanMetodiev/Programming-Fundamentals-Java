9e0c270001b43253c873b355f725a2e7013cb1ac
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-11-04 22:28:20 +0200

    Update CaesarCipher.java
    delete single line

--- Text Processing - Exercise/CaesarCipher.java
+++ Text Processing - Exercise/CaesarCipher.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class CaesarCipher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String text = scanner.nextLine();
         for (int i = 0; i < text.length(); i++) {
             System.out.printf("%c", text.charAt(i) + 3);
