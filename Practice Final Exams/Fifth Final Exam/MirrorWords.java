caa2e68654373634aca0da938ec19e154eb2a27e
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-11-16 23:34:15 +0200

    Update MirrorWords.java

--- Practice Final Exams/Fifth Final Exam/MirrorWords.java
+++ Practice Final Exams/Fifth Final Exam/MirrorWords.java
@@ -7,12 +7,10 @@ import java.util.regex.Pattern;
 public class MirrorWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String input = scanner.nextLine();
 
         Pattern pattern = Pattern.compile("([@|#])[A-Za-z]{3,}\\1\\1[A-Za-z]{3,}\\1");
         Matcher matcher = pattern.matcher(input);
-
 
         int counter = 0;
         List<String> wordList = new ArrayList<>();
@@ -31,7 +29,6 @@ public class MirrorWords {
             }
         }
         if (counter > 0) {
-
             System.out.printf("%d word pairs found!\n", counter);
         }else{
             System.out.println("No word pairs found!");
@@ -42,20 +39,17 @@ public class MirrorWords {
 
             System.out.println("The mirror words are:");
             while (!wordList.isEmpty()) {
-
+                
                 resultText += String.format("%s <=> %s, ", wordList.get(0), wordList.get(1));
                 wordList.remove(0);
                 wordList.remove(0);
             }
             resultText = resultText.replaceAll("[#@]", "");
-
             int lastIndex = resultText.lastIndexOf(",");
             resultText = resultText.substring(0, lastIndex);
             System.out.println(resultText);
         } else  {
-
             System.out.println("No mirror words!");
         }
-
     }
 }
