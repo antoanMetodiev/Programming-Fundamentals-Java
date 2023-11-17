2dbd1e234f0b960b95de5b4d736072fdb038d99c
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-11-17 21:10:17 +0200

    Update AppendArrays.java

--- Lists/Lists - Exercise/AppendArrays.java
+++ Lists/Lists - Exercise/AppendArrays.java
@@ -5,11 +5,8 @@ public class AppendArrays {
         String[] numbers = scanner.nextLine().split("\\s*\\|\\s*");
 
         for (int i = numbers.length - 1; i >= 0; i--) {
-
             String[] arr = numbers[i].split("\\s+");
-
             for (int j = 0; j < arr.length; j++) {
-
                 System.out.print(arr[j] + " ");
             }
         }
