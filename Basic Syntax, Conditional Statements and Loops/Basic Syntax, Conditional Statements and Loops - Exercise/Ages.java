d537d613acc4a2121a8a942ead37825890323f4f
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-11-20 20:54:22 +0200

    Update Ages.java

--- Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Ages.java
+++ Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - Exercise/Ages.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class Ages {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int ages = Integer.parseInt(scanner.nextLine());
@@ -8,17 +7,13 @@ public class Ages {
 
         if (ages >= 0 && ages <= 2){
             stage = "baby";
-        }
-        else if (ages >= 3 && ages <= 13){
+        } else if (ages >= 3 && ages <= 13){
             stage = "child";
-        }
-        else if (ages >= 14 && ages <= 19){
+        }else if (ages >= 14 && ages <= 19){
             stage = "teenager";
-        }
-        else if (ages >= 20 && ages <= 65){
+        }else if (ages >= 20 && ages <= 65){
             stage = "adult";
-        }
-        else if (ages >= 66){
+        }else if (ages >= 66){
             stage = "elder";
         }
         System.out.println(stage);
