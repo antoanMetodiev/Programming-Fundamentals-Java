b93e70116917263af4a4575b25cc9a7d3a361737
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2023-11-20 20:52:29 +0200

    Update BeerKegs.java

--- Data Types and Variables/Data Types and Variables - Exercise/BeerKegs.java
+++ Data Types and Variables/Data Types and Variables - Exercise/BeerKegs.java
@@ -4,15 +4,13 @@ public class BeerKegs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int botles = Integer.parseInt(scanner.nextLine());
-
         double maxValue = 0;
         String winnerBotle = "";
         for (int i = 1; i <= botles ; i++) {
-
+            
             String botleModel = scanner.nextLine();
             double r = Double.parseDouble(scanner.nextLine());
             int h = Integer.parseInt(scanner.nextLine());
-
             double result = Math.PI * (r * r) * h;
             if (result > maxValue){
                 maxValue = result;
